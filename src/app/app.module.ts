a2fa1ff9a358100900d5a5851ea657cb405f5a7e
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2018-09-18 22:49:30 -0400

    test
    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import to use routes within application
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { CommunityComponent } from './community/community.component';
+import { NewsComponent } from './news/news.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+
+//constant to house all routes
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    NavigationComponent,
+    AboutComponent,
+    HomeComponent,
+    CommunityComponent,
+    NewsComponent,
+    DropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
